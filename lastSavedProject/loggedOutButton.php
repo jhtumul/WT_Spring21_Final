2b2c4249213bc83fffbdb6015b120c728d18dd04
jhtumul <jhtumul@gmail.com>
2021-04-20 21:50:09 +0600

    deleting

--- lastSavedProject/loggedOutButton.php
+++
@@ -1,5 +0,0 @@
-<head>
-<link rel="stylesheet" href="styles/basicLayout.css">
-</head>
-<li><a href="registration.php" class="right">Registration</a></li>
-<li><a href="login.php" class="right">Login</a></li>
--- lastSavedProject/logout.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    session_start();
-    unset($_SESSION["user"]);
-    unset($_SESSION["type"]);
-    unset($_SESSION["pass"]);
-    header("Location: index.php")
-?>
