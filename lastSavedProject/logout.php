7c83c3343d6831121e5f0c30f271184ad7580c9b
jhtumul <jhtumul@gmail.com>
2021-04-21 13:33:22 +0600

    deleting

--- lastSavedProject/logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    session_start();
-    // unset($_SESSION["user"]);
-    // unset($_SESSION["type"]);
-    // unset($_SESSION["pass"]);
-    session_destroy();
-    header("Location: index.php")
-?>
