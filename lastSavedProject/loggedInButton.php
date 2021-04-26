e19f9dbe040e631a214a2d0b8947fb16e28441b6
jhtumul <jhtumul@gmail.com>
2021-04-27 03:07:02 +0600

    deleting

--- lastSavedProject/loggedInButton.php
+++
@@ -1,6 +0,0 @@
-<head>
-<link rel="stylesheet" href="styles/basicLayout.css">
-</head>
-<li><a href="myAccount.php" class="right">Account</a></li>
-<li><a href="logout.php" class="right">Logout</a></li>
-<li><a href="#" class="right set-cart" style="background-color:whitesmoke; color:black; border-radius:2px"><img src="images/shopping-cart.png" width="25" height="25" alt=""><br><span style="font-size: small;">item(0)</span></a></li>
