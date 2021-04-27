3f439a4ac788c732d4b3ebfc7ca0375deca60e42
jhtumul <jhtumul@gmail.com>
2021-04-27 20:36:07 +0600

    uploading lastsaved project

---
+++ lastSavedProject/admin_header.php
@@ -0,0 +1,28 @@
+<?php
+	session_start();
+	if($_SESSION["type"] != "admin")
+	{
+		header("Location: index.php");
+	}
+                
+?>
+<html>
+
+	<head>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="styles/mystyle.css">
+	</head>
+	<body>
+		<div class="text-center">
+			<h1 class="header">Admin</h1>
+		</div>
+		<!--menu starts-->
+		<div class="text-center">
+			<a href="dashboard.php" class="btn btn-primary">Dashboard</a>
+			<a href="allproducts.php" class="btn btn-warning">All Products</a>
+			<a href="addproduct.php" class="btn btn-danger">Add Product</a>
+			<a href="allcategories.php" class="btn btn-warning">All Categories</a>
+			<a href="addcategory.php" class="btn btn-info">Add Category</a>
+			<a href="logout.php" class="btn btn-danger">Logout</a>		
+		</div>
+		<!--menu ends-->
